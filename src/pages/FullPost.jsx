b000f37af436617e44bff1d22c18d74d9f9e86fc
NazarSavchuk <nazarsavcuk3@gmail.com>
2022-10-22 15:55:52 +0200

    auth functional

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -3,25 +3,47 @@ import React from "react";
 import { Post } from "../components/Post";
 import { Index } from "../components/AddComment";
 import { CommentsBlock } from "../components/CommentsBlock";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export const FullPost = () => {
+  const [data, setData] = React.useState();
+  const [isLoading, setIsLoading] = React.useState(true);
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    axios
+      .get(`http://localhost:4444/posts/${id}`)
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Error");
+      });
+  }, []);
+
+  if (isLoading) {
+    return <Post isLoading={isLoading} />;
+  }
+  console.log(data.user.fullName);
+
   return (
     <>
       <Post
-        id={1}
-        title="Roast the code #1 | Rock Paper Scissors"
-        imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+        id={data.id}
+        title={data.title}
+        imageUrl={data.imageUrl}
         user={{
-          avatarUrl:
-            "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-          fullName: "Keff",
+          avatarUrl: data.user.avatarUrl,
+          fullName: data.user.fullName,
         }}
-        createdAt={"12 июня 2022 г."}
-        viewsCount={150}
-        commentsCount={3}
+        createdAt={data.createdAt}
+        viewsCount={data.viewsCount}
+        commentsCount={data.commentsCount}
         tags={["react", "fun", "typescript"]}
-        isFullPost
-      >
+        isFullPost>
         <p>
           Hey there! 👋 I'm starting a new series called "Roast the Code", where
           I will share some code, and let YOU roast and improve it. There's not
@@ -47,8 +69,7 @@ export const FullPost = () => {
             text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
           },
         ]}
-        isLoading={false}
-      >
+        isLoading={false}>
         <Index />
       </CommentsBlock>
     </>
